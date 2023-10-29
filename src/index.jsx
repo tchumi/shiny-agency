532af5929b2520da12f3302692e414eacbf9f2f1
Michel <michel.kleck@neuf.fr>
2023-10-29 09:16:20 +0100

    first commit in branch outlet

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
-import Header from './components/Header/'
-import Home from './pages/Home/'
-import Survey from './pages/Survey/'
+import Header from './components/Header'
+import Home from './pages/Home'
+import Survey from './pages/Survey'
+// On ajoute nos composants
+import ClientForm from './components/ClientForm'
+import FreelanceForm from './components/FreelanceForm'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +14,11 @@ ReactDOM.render(
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/survey" element={<Survey />} />
+        <Route path="/survey" element={<Survey />}>
+          {/* Nous imbriquons nos composants dans survey */}
+          <Route path="client" element={<ClientForm />} />
+          <Route path="freelance" element={<FreelanceForm />} />
+        </Route>
       </Routes>
     </Router>
   </React.StrictMode>,
