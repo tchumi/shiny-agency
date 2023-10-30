8dce32b712c60d8416457951676190325f2a700a
Michel <michel.kleck@neuf.fr>
2023-10-30 16:30:02 +0100

    P1C2 solution MK

--- src/pages/Survey/index.jsx
+++ src/pages/Survey/index.jsx
@@ -1,12 +1,32 @@
-import { useParams } from 'react-router-dom'
+import { useState } from 'react'
+import { Link, useParams } from 'react-router-dom'
 
 function Survey() {
   const { questionNumber } = useParams()
+  const [question, setQuestion] = useState(1)
+  const [goToResults, setGoToResults] = useState(false)
 
-  return (
+  return goToResults ? (
     <div>
       <h1>Questionnaire 🧮</h1>
-      <h2>Question {questionNumber}</h2>
+      <h2>Fin du questionnaire</h2>
+      <Link to="/results">Résultats</Link>
+    </div>
+  ) : (
+    <div>
+      <h1>Questionnaire 🧮</h1>
+      <h2>Question {question}</h2>
+      <button onClick={() => question > 1 && setQuestion(question - 1)}>
+        Question précédente
+      </button>
+      {question === 10 && (
+        <button onClick={() => setGoToResults(true)}>Résultats</button>
+      )}
+      {question < 10 && (
+        <button onClick={() => setQuestion(question + 1)}>
+          Question suivante
+        </button>
+      )}
     </div>
   )
 }
