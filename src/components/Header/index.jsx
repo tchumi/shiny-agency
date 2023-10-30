b81ed57383a05457f9f1b5ba32b5c9a3fa89ae8e
Michel <michel.kleck@neuf.fr>
2023-10-30 16:30:15 +0100

    P1C2 solution MK

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,6 +5,8 @@ function Header() {
     <nav>
       <Link to="/">Accueil</Link>
       <Link to="/survey/42">Questionnaire</Link>
+      <Link to="/results">Résultats</Link>
+      <Link to="/freelances">Freelances</Link>
     </nav>
   )
 }
--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom'
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import Error from './components/Error/'
 import Header from './components/Header/'
+import Freelances from './pages/Freelances/'
 import Home from './pages/Home/'
+import Results from './pages/Results/'
 import Survey from './pages/Survey/'
 
 ReactDOM.render(
@@ -13,6 +15,8 @@ ReactDOM.render(
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/survey/:questionNumber" element={<Survey />} />
+        <Route path="/Results/" element={<Results />} />
+        <Route path="/Freelances/" element={<Freelances />} />
         <Route path="*" element={<Error />} />
       </Routes>
     </Router>
