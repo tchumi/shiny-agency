c33e9b6b98a65350c56d747df7716311c228c8e7
Michel <michel.kleck@neuf.fr>
2023-11-06 10:50:05 +0100

    commit survey

--- src/pages/Survey/index.jsx
+++ src/pages/Survey/index.jsx
@@ -1,21 +1,35 @@
 import { Link, useParams } from 'react-router-dom'
 import PageWrapper from '../../utils/style/PageWrapper'
+import { useEffect } from 'react'
+import { useState } from 'react'
 
 function Survey() {
   const { questionNumber } = useParams()
   const questionNumberInt = parseInt(questionNumber)
   const prevQuestionNumber = questionNumberInt === 1 ? 1 : questionNumberInt - 1
   const nextQuestionNumber = questionNumberInt + 1
+  const [surveyData, setSurveyData] = useState([])
+
+  useEffect(() => {
+    fetch(`http://localhost:8000/survey`).then((response) =>
+      response
+        .json()
+        .then(({ surveyData }) => setSurveyData(surveyData))
+        .catch((error) => console.log(error)),
+    )
+  }, [])
+
   return (
     <PageWrapper>
       <div>
         <h1>Questionnaire 🧮</h1>
         <h2>Question {questionNumber}</h2>
+        <h3>{surveyData[questionNumber]} </h3>
         <Link to={`/survey/${prevQuestionNumber}`}>Précédent</Link>
-        {questionNumberInt === 10 ? (
-          <Link to="/results">Résultats</Link>
-        ) : (
+        {surveyData[questionNumberInt + 1] ? (
           <Link to={`/survey/${nextQuestionNumber}`}>Suivant</Link>
+        ) : (
+          <Link to="/results">Résultats</Link>
         )}
       </div>
     </PageWrapper>
